155ee6bb0b8ebecf4c05a4383e4a39d93aa1266a
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-01-29 15:15:55 -0300

    feat(board): initial board structure, section component and style improvements
    - Add Section component for board columns
    - Add utility for className merging (tw-merge)
    - Update layout and page to use new board structure
    - Add and configure new dependencies: clsx, tailwind-merge, lucide-react
    - Improve global styles with custom navy color palette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import { ArchiveIcon } from "lucide-react";
+import { Section } from "@/components/section"
+
 export default function Home() {
+
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <div className="max-w-405 w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
+      <div />
+      <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+        <Section.Root>
+          <Section.Header>
+            <Section.Title>
+              <ArchiveIcon className="size-3" />
+              Backlog
+            </Section.Title>
+            <Section.IssueCount>
+              10
+            </Section.IssueCount>
+          </Section.Header>
+          <Section.Content>
+            <div>card 1</div>
+            <div>card 2</div>
+            <div>card 3</div>
+            <div>card 4</div>
+          </Section.Content>
+        </Section.Root>
+      </main>
+    </div>
   );
 }
