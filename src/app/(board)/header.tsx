44eca67d1ca8b662338535064d8b189624300f0c
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-01-30 09:26:00 -0300

    feat(board): update header component

--- src/app/(board)/header.tsx
+++ src/app/(board)/header.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import { Input } from "@/components/input";
 import { LogInIcon, SearchIcon } from "lucide-react";
+import { parseAsString, useQueryState, debounce } from "nuqs";
+import { ChangeEvent } from "react";
 
 
 export function Header() {
+  const [search, setSearch] = useQueryState('q', parseAsString.withDefault(''));
+
+  function handleSearchUpdate(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value, {
+      limitUrlUpdates: event.target.value !== '' ? debounce(500) : undefined,
+    });
+  }
+
   return (
     <div className="max-w-225 mx-auto w-full flex items-center justify-between">
       <div className="space-y-1">
@@ -18,6 +30,8 @@ export function Header() {
             className="w-67.5 pl-8"
             type="text"
             placeholder="Search for features"
+            value={search}
+            onChange={handleSearchUpdate}
           />
         </div>
         <button
