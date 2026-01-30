0b40684d074587d500e0b61eda7f72c8c0f1a013
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-01-30 09:50:13 -0300

    feat(app): update header to reflect authenticated user state

--- src/app/(board)/header.tsx
+++ src/app/(board)/header.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import { Input } from "@/components/input";
-import { LogInIcon, SearchIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { Loader2Icon, LogInIcon, SearchIcon } from "lucide-react";
 import { parseAsString, useQueryState, debounce } from "nuqs";
 import { ChangeEvent } from "react";
 
 
 export function Header() {
+  const { data: session, isPending } = authClient.useSession()
   const [search, setSearch] = useQueryState('q', parseAsString.withDefault(''));
+
+  const isAuthenticated = !isPending && session?.user;
 
   function handleSearchUpdate(event: ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value, {
       limitUrlUpdates: event.target.value !== '' ? debounce(500) : undefined,
     });
+  }
+
+  async function handleSignIn() {
+    await authClient.signIn.social({ provider: 'github', callbackURL: '/' });
+  }
+
+  async function handleSignOut() {
+    await authClient.signOut();
   }
 
   return (
@@ -34,12 +46,42 @@ export function Header() {
             onChange={handleSearchUpdate}
           />
         </div>
-        <button
-          className="size-8 rounded-full bg-navy-700 border border-navy-500 flex items-center justify-center hover:bg-navy-600 transition-colors duration-150"
-          type="button"
-        >
-          <LogInIcon className="size-3.5 text-navy-200" />
-        </button>
+        {
+          isPending && (
+            <div
+              className="size-8 rounded-full bg-navy-700 border border-navy-500 flex items-center justify-center"
+            >
+              <Loader2Icon className="size-3.5 text-navy-200 animate-spin" />
+            </div>
+          )
+        }
+        {
+          isAuthenticated && (
+            <button
+              className="size-8 rounded-full overflow-hidden cursor-pointer"
+              type="button"
+              onClick={handleSignOut}
+              disabled={isPending}
+            >
+              <img
+                className="size-8 rounded-full"
+                src={session.user.image ?? ''}
+                alt={session.user.name}
+              />
+            </button>
+          )
+        }
+        {
+          !isAuthenticated && (
+            <button
+              className="size-8 rounded-full bg-navy-700 border border-navy-500 flex items-center justify-center hover:bg-navy-600 transition-colors duration-150"
+              type="button"
+              onClick={handleSignIn}
+              disabled={isPending}
+            >
+              <LogInIcon className="size-3.5 text-navy-200" />
+            </button>
+          )}
       </div>
     </div>
   )
