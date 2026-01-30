ae312388787c7744b11b1cb583e36211a33b68dc
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-01-30 16:05:32 -0300

    style(issue): sort and group imports
    Sort and group imports in src/app/issues/[id]/page.tsx for better readability and maintainability.

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getIssue } from "@/http/get-issue";
 import Link from "next/link";
 import { ArchiveIcon, MoveLeftIcon, ThumbsUpIcon } from "lucide-react";
 import { Button } from "@/components/button";
+import { IssueCommentsList } from "./issue-comments/issue-comments-list";
 
 interface IssuePageProps {
   params: Promise<{ id: string }>;
@@ -57,6 +58,14 @@ export default async function IssuePage({ params }: IssuePageProps) {
         <p className="text-navy-100 text-sm leading-relaxed">
           {issue.description}
         </p>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <span className="font-semibold">Comments</span>
+        <form />
+        <div className="mt-3">
+          <IssueCommentsList issueId={issue.id} />
+        </div>
       </div>
     </main>
   )
